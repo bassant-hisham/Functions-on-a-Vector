164c55089a46ce1256496a2e72617d83a9111e4d
bassant-hisham <bassentkhafagi@gmail.com>
2023-10-27 00:09:47 +0200

    Average vector Elements

--- MakeSum.cpp
+++ MakeSum.cpp
@@ -2,12 +2,17 @@
 #include<vector>
 using namespace std;
 int getSumOfVectorElements(vector<int>);
+int getAverageOfVectorElements(vector<int>);
 
 int main()
 {
 	vector<int> vectorToBeSummed = {1,2,3,4,5,6,7,8,9,10};
+
 	int accumlatedSum = getSumOfVectorElements(vectorToBeSummed);
 	cout<<"The Sum Of the Vector Elements {1,2,3,4,5,6,7,8,9,10} = "<<accumlatedSum<<endl;
+
+	int AverageOfVectorElements = getAverageOfVectorElements(vectorToBeSummed);
+	cout<<"The Average Of the Vector Elements {1,2,3,4,5,6,7,8,9,10} = "<<AverageOfVectorElements<<endl;
 	return 0;
 }
 
@@ -21,4 +26,15 @@ int getSumOfVectorElements(vector<int> vectorToBeSummed)
 	}
 
 	return accumlatedSum;
+}
+
+int getAverageOfVectorElements(vector<int> vectorToBeSummed)
+{
+	int accumlatedSum = 0;
+	for (int i = 0 ; i < vectorToBeSummed.size() ; i++) 
+	{ 
+		accumlatedSum += vectorToBeSummed[i];
+	}
+	int averageOfVector=accumlatedSum/vectorToBeSummed.size();
+	return averageOfVector;
 }
